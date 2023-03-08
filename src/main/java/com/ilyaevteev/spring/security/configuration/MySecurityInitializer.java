4543a9e4a7c7bc8a9ea838d50abf6b5863882e49
Ilya Evteev <evteev-ilya@inbox.ru>
2023-03-08 13:57:06 +0300

    first commit

---
+++ src/main/java/com/ilyaevteev/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.ilyaevteev.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
